da88ba2bc8b00232c52faeea0620351df7aa3e11
COLONELMAJOR777 <151913831+COLONELMAJOR777@users.noreply.github.com>
2024-03-20 18:16:59 +0300

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['0115138630']
+global.ownername = "العقيد الرائد 777"//owner name
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
 global.location = "Indian"
